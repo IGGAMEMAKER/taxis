d04e2a1c9c42806b744906965d0a2a38a3e8b849
IGGAMEMAKER <23i03g@mail.ru>
2016-12-27 21:51:08 +0300

    tester module

---
+++ helpers/tester.js
@@ -0,0 +1,44 @@
+var request = require('superagent');
+var Promise = require('bluebird');
+
+var logger = require('../helpers/logger');
+var domain = 'http://localhost:8888';
+
+var api = require('../helpers/api');
+
+var log = tag => (err, res) => {
+  if (err) {
+    logger.log('ERROR in ' + tag, err);
+    logger.log('ERROR in ' + tag, err.status);
+  } else {
+    logger.log(tag + ' OK', res.body);
+  }
+};
+
+var get = (url, tag) => {
+  request.get(domain + url)
+    .end(log(url));
+};
+
+var post = (url, obj, tag) => {
+  request.post(domain + url)
+    .send(obj)
+    .end(log(url));
+};
+
+var patch = (url, obj, tag) => {
+  request.patch(domain + url)
+    .send(obj)
+    .end(log(url));
+};
+
+var authenticateAsUser = (object) => Object.assign({ phone: '8955555555', authKey: 'qXtvs1029dasi0w' }, object);
+
+module.exports = {
+  get,
+  post,
+  patch,
+  api,
+  logger,
+  authenticateAsUser
+};
--- tests/test-api.js
+++ tests/test-api.js
@@ -1,37 +1,4 @@
-var request = require('superagent');
-var Promise = require('bluebird');
-
-var logger = require('../helpers/logger');
-var domain = 'http://localhost:8888';
-
-var api = require('../helpers/api');
-
-var log = tag => (err, res) => {
-  if (err) {
-    logger.log('ERROR in ' + tag, err);
-    logger.log('ERROR in ' + tag, err.status);
-  } else {
-    logger.log(tag + ' OK', res.body);
-  }
-};
-
-var get = (url, tag) => {
-  request.get(domain + url)
-    .end(log(url));
-};
-
-var post = (url, obj, tag) => {
-  request.post(domain + url)
-    .send(obj)
-    .end(log(url));
-};
-
-var patch = (url, obj, tag) => {
-  request.patch(domain + url)
-    .send(obj)
-    .end(log(url));
-};
-
+var tester = require('../helpers/tester');
 
 // get('/users/all');
 // post('/users', { phone: '8955555555131', name: 'Gaga1222' });
@@ -119,4 +86,4 @@ post('/drivers', {
 });
 // */
 
-get('/orders/drivers?phone=8955555555&authKey=qXtvs1029dasi0w');
+tester.get('/orders/drivers?phone=8955555555&authKey=qXtvs1029dasi0w');
