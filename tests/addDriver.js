8b51748c10193f5450e5a24ca982481c5ec2feae
IGGAMEMAKER <23i03g@mail.ru>
2017-01-19 17:40:06 +0300

    driver test

---
+++ tests/addDriver.js
@@ -0,0 +1,17 @@
+var tester = require('../helpers/tester');
+
+tester.post('/drivers', {
+  phone: '8955555555132', name: 'Алексей Доронин',
+  mark: 7,
+  age: 25,
+  experience: 4,
+  gender: 1,
+  categories: {},
+  internationalLicence: 1,
+  isOfficial: 1,
+  isSmoking: 0,
+  isSessionOpened: 1
+})
+.then(r => {
+  console.log('result', r);
+});
