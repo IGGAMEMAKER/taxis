19d568c2a23254d2694805a59594a307b06e6eba
IGGAMEMAKER <23i03g@mail.ru>
2017-01-22 13:12:39 +0300

    ping drivers

--- routes/index.js
+++ routes/index.js
@@ -40,7 +40,7 @@ router.get('/events', (req, res) => {
   });
 });
 
-router.get('/driver-test/:id', (req, res) {
+router.get('/driver-test/:id', (req, res) => {
   orderNotifier.pingDriverChannel(req.params.id, { ggg: 1 })
     .then(r => {
       res.render('order-test')
