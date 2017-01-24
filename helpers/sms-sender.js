52b7c4957a9754353b3f2ed3cccbffaaaae8b022
IGGAMEMAKER <23i03g@mail.ru>
2017-01-24 16:48:16 +0300

    almost done

--- helpers/sms-sender.js
+++ helpers/sms-sender.js
@@ -1,55 +1,104 @@
 const configs = require('../configs');
 
-// const smsApiKey = configs.smsApiKey;
-// const smsApiSecret = configs.smsApiSecret;
-const smsApiKey = '5F5C418A0F914BBC8234A9BF5EDDAD97';
-const smsApiSecret = 'JViE5vDor0Sw3WllZka15Q==';
 
+const smsApiKey = configs.smsApiKey;
+const smsApiSecret = configs.smsApiSecret;
+// const smsApiKey = '5F5C418A0F914BBC8234A9BF5EDDAD97';
+// const smsApiSecret = 'JViE5vDor0Sw3WllZka15Q==';
+
+const sinchSms = require('sinch-sms')({
+  key: smsApiKey,
+  secret: smsApiSecret
+});
 
 const Promise = require('bluebird');
 
 const logger = require('../helpers/logger');
 
-const request = require('superagent');
-
-const crypto = require('crypto');
-
 // const parseToUTF8 = (myString) => JSON.parse(JSON.stringify(myString));
 const parseToUTF8 = (myString) => JSON.stringify(myString);
-
-module.exports = (sender, recipient, message, options) => {
-  const body = message;
-  const ContentMD5 = crypto.createHash('md5').update(parseToUTF8(body)).digest('base64');
-  // const ContentMD5 = 'jANzQ+rgAHyf1MWQFSwvYw==';
-  logger.log('ContentMD5')
-  logger.log('\t', ContentMD5);
-
-  // const xTimestamp = new Date();
-  const xTimestamp = '2014-06-04T13:41:58Z';
-  const timestamp = `x-timestamp:${xTimestamp}`;
-  const contentType = 'application/json';
-  const StringToSign = `POST\n${ContentMD5}\n${contentType}\n${timestamp}\n/v1/sms/${recipient}`;
-  logger.log('StringToSign')
-  logger.log('\t', StringToSign);
-
-  const secret = smsApiSecret;
-  // const secret = 'SlZpRTV2RG9yMFN3M1dsbFprYTE1UT09';
-  const Signature = crypto.createHmac('sha256', secret)
-    .update(parseToUTF8(StringToSign))
-    .digest('base64');
-  logger.log('Signature')
-  logger.log('\t', Signature);
-
-  const Authorization = `Application ${smsApiKey}:${Signature}`;
-  logger.log('Authorization')
-  logger.log('\t', Authorization);
-
-  // return request
-  //   .post(`https://messagingapi.sinch.com/v1/Sms/${recipient}`)
-  //   .send({ from: sender || 'Трезвый водитель', message })
-  //   .set('Authorization', Authorization)
-  //   .set('X-Timestamp', xTimestamp)
-  //   .set('Content-Type', contentType)
-  //   .then(r => r)
-  //   .catch(err => { throw err; });
+// 143460337
+const send = (sender, recipient, message, options) => {
+  return sinchSms.send(recipient, message)
+    .then(result => {
+      logger.log('SMS Response', result);
+      return result;
+    })
+    .catch(err => {
+      logger.log('SMS Error', err);
+    });
 };
+
+const check = (messageId) => {
+  return sinchSms.getStatus(messageId)
+    .then(result => {
+      logger.log('SMS GET STATUS RESPONSE', result);
+      return result;
+    })
+    .catch(err => {
+      logger.log('SMS GET STATUS Error', err);
+    });
+};
+
+const cycleGettingStatus = (messageId, period) => {
+  check(messageId);
+  setTimeout(() => {
+    cycleGettingStatus(messageId, period);
+  }, period);
+};
+
+const pend = (sender, recipient, message, options) => {
+  let messageId;
+  send(sender, recipient, message, options)
+    .then(result => {
+      messageId = result.messageId;
+
+      cycleGettingStatus(messageId, 1000);
+    });
+}
+
+module.exports = {
+  send,
+  check,
+  pend
+};
+  // const body = message;
+  // const ContentMD5 = crypto.createHash('md5').update(parseToUTF8(body)).digest('base64');
+  // // const ContentMD5 = 'jANzQ+rgAHyf1MWQFSwvYw==';
+  // logger.log('ContentMD5')
+  // logger.log('\t', ContentMD5, ContentMD5 === 'jANzQ+rgAHyf1MWQFSwvYw==' ? 'correct' : 'incorrect');
+  //
+  // // const xTimestamp = new Date();
+  // const xTimestamp = '2014-06-04T13:41:58Z';
+  // const timestamp = `x-timestamp:${xTimestamp}`;
+  // const contentType = 'application/json';
+  // const StringToSign = `POST\n${ContentMD5}\n${contentType}\n${timestamp}\n/v1/sms/${recipient}`;
+  // logger.log('StringToSign')
+  // logger.log('\t', StringToSign);
+  //
+  // // const secret = smsApiSecret;
+  // // const secret = 'SlZpRTV2RG9yMFN3M1dsbFprYTE1UT09';
+  // const secret = 'JViE5vDor0Sw3WllZka15Q==';
+  // // const secret = 'SlZpRTV2RG9yMFN3M1dsbFprYTE1UT09';
+  // const Signature = crypto.createHmac('sha256', secret)
+  //   .update(StringToSign)
+  //   .digest('base64');
+  // logger.log('Signature')
+  // logger.log('\t', Signature);
+  //
+  // const Authorization = `Application ${smsApiKey}:${Signature}`;
+  // logger.log('Authorization')
+  // logger.log('\t', Authorization);
+  // // SlZpRTV2RG9yMFN3M1dsbFprYTE1UT09
+  //
+  // // const ANSWER =
+  //
+  // // return request
+  // //   .post(`https://messagingapi.sinch.com/v1/Sms/${recipient}`)
+  // //   .send({ from: sender || 'Трезвый водитель', message })
+  // //   .set('Authorization', Authorization)
+  // //   .set('X-Timestamp', xTimestamp)
+  // //   .set('Content-Type', contentType)
+  // //   .then(r => r)
+  // //   .catch(err => { throw err; });
+// };
--- tests/sms-library-test.js
+++ tests/sms-library-test.js
@@ -1,5 +1,6 @@
 const smsSender = require('../helpers/sms-sender');
 
 // smsSender('All Drivers', '+79165351652', 'Sms test', {});
-// smsSender('All Drivers', '+46700000000', 'Hello world', {});
-smsSender('All Drivers', '+46700000000', {"message":"Hello world"}, {});
+smsSender.pend('All Drivers', '+79165351652', 'Hello world', {});
+// smsSender.check(143461856);
+// smsSender('All Drivers', '+46700000000', {"message":"Hello world"}, {});
